41912586760dd4ea516565803ce77cde2ddb4d6c
rafai <rafac11@hotmail.com>
2017-10-10 14:12:03 -0500

    se modifico la forma de acceso

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-var port=porcess.env.PORT || 3000;
+var port=process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
